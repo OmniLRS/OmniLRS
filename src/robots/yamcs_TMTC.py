ff9d50aa136cd60c288336c058474fdf1bd6b059
Aksont <aastanivuk@gmail.com>
2025-11-14 06:07:48 +0000

    extracted TMTC in a separate file

---
+++ src/robots/yamcs_TMTC.py
@@ -0,0 +1,61 @@
+__author__ = "Aleksa Stanivuk"
+__status__ = "development"
+
+from src.environments.utils import transform_orientation_into_xyz
+from yamcs.client import YamcsClient
+import threading
+import time
+import numpy as np
+
+class YamcsTMTC:
+    """
+    YamcsTMTC class.
+    It allows to control a robot instance, by subscribing and reacting to TMTC instructions coming from Yamcs client.
+    It also publishes relevant robot information to Yamcs client.
+    """
+
+    def __init__(
+        self,
+        yamcs_conf,
+        robot_name,
+        robot_RG
+    ) -> None:
+        #TODO use configs
+        self._yamsc_client = YamcsClient('localhost:8090')
+        self._yamsc_processor = self._yamsc_client.get_processor(instance='workshop', processor='realtime')
+        self._mdb = self._yamsc_client.get_mdb(instance="workshop")  #NOTE https://docs.yamcs.org/python-yamcs-client/mdb/client/#yamcs.client.MDBClient
+        self._robot_name = robot_name
+        self._robots_RG = robot_RG
+
+    def start(self):
+        #TODO take seconds to wait out of config
+        # initially inteded to be in a for robot in robots loop, thus to have one thread for each robot
+        # however, for the workshop use-case, the code was simplified to assume use of only one robot 
+        seconds = 3 #TODO get them from the config
+        t = threading.Thread(
+            target=self._yamcs_transmitter,
+            args=(self._robot_name, seconds),
+            name="yamcs-TMTC-" + self._robot_name,
+            daemon=True,
+        )   
+        t.start()
+
+    def _yamcs_transmitter(self, robot_name, interval_s):
+        print("started transmitter for: " + robot_name)
+        try:
+            while True:
+                self._transmit_ground_pose()
+                # add here further commands
+                time.sleep(interval_s) #TODO: change into simulation secs
+        finally:
+            print("ended transmitter for: " + robot_name)
+
+    def _transmit_ground_pose(self):
+        position, orientation = self._robots_RG[str(self._robot_name)].get_base_pose()
+        # euler_orient = transform_orientation_into_xyz(orientation)
+        position = [round(x, 1) for x in position.tolist()]         # position = position.tolist()
+        orientation = [round(x, 1) for x in orientation.tolist()]   # orientation = orientation.tolist()
+        ground_pose_truth = {"position": {"x":position[0], "y":position[1], "z":position[2]}, 
+                                "orientation":{"w":orientation[0],"x":orientation[1], "y":orientation[2], "z":orientation[3] }}
+        self._yamsc_processor.set_parameter_value(f"/Rover/pose_ground_truth", ground_pose_truth)
+
