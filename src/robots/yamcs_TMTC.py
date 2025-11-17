8b149bf1f106acd594a9b454ca4b264e432de7a0
Aksont <aastanivuk@gmail.com>
2025-11-17 13:51:26 +0000

    removed rounding for position and orientation

--- src/robots/yamcs_TMTC.py
+++ src/robots/yamcs_TMTC.py
@@ -49,8 +49,8 @@ class YamcsTMTC:
     def _transmit_base_link_pose(self):
         position, orientation = self._robots_RG[str(self._robot_name)].get_base_link_pose()
         # euler_orient = transform_orientation_into_xyz(orientation)
-        position = [round(x, 1) for x in position.tolist()]         # position = position.tolist()
-        orientation = [round(x, 1) for x in orientation.tolist()]   # orientation = orientation.tolist()
+        position = position.tolist()
+        orientation = orientation.tolist()
         pose_of_base_link = {"position": {"x":position[0], "y":position[1], "z":position[2]}, 
                                 "orientation":{"w":orientation[0],"x":orientation[1], "y":orientation[2], "z":orientation[3] }}
         self._yamcs_processor.set_parameter_value(self._yamcs_conf["parameters"]["pose_of_base_link"], pose_of_base_link)
