59cc8316fb98691cc24449604c2971cfc38da3e7
Amaan Javed <amaan@missioncontrolspaceservices.com>
2026-06-11 11:17:16 -0400

    Update contribution comments

--- src/mission_specific/husky/subsystems/husky_power_model.py
+++ src/mission_specific/husky/subsystems/husky_power_model.py
@@ -3,6 +3,8 @@ __copyright__ = "Copyright 2026, JAOPS"
 __license__ = "BSD-3-Clause"
 __version__ = "3.0.0"
 __status__ = "development"
+__maintainer__ = "Louis Burtz"
+__email__ = "ljburtz@jaops.com"
 
 from enum import Enum
 from typing import Tuple
