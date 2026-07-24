108d301e4328a47695db72a18d77c98356624b49
Tomo Norman <tomo.l.norman@gmail.com>
2026-07-24 13:13:49 +0900

    clarification on pytest_configure

--- test/conftest.py
+++ test/conftest.py
@@ -2,8 +2,7 @@ import os
 
 
 def pytest_configure(config):
-    """Write a JUnit XML report into the Artefacts upload dir when running
-    under an Artefacts scenario (equivalent to passing --junit-xml)."""
+    """junit output path for when using with artefacts"""
     upload_dir = os.environ.get("ARTEFACTS_SCENARIO_UPLOAD_DIR")
     if upload_dir and not config.option.xmlpath:
         config.option.xmlpath = os.path.join(upload_dir, "tests_junit.xml")
